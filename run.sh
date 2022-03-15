e765036abfa470661c1c30f1986c711b1458f04f
Prokopkin Sergey <71408915+prokopk1n@users.noreply.github.com>
2022-03-15 19:04:39 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p outputs
-make gk
-for file in tests/*.txt; do
-  filename=$(basename "$file")
-  echo ========== TEST "$filename" ==========
-  ./gk < tests/"$filename" > outputs/"$filename"
-  python3 check_tests.py answers/"$filename" outputs/"$filename"
-done
