15f1a65c846ff1f76c8ecabf2a650bfa06fa1768
Prokopkin Sergey <71408915+prokopk1n@users.noreply.github.com>
2022-03-15 17:19:19 +0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ for file in tests/*.txt; do
   filename=$(basename "$file")
   echo ========== TEST "$filename" ==========
   ./gk < tests/"$filename" > outputs/"$filename"
-  python3 tests/check_tests.py answers/"$filename" outputs/"$filename"
+  python3 check_tests.py answers/"$filename" outputs/"$filename"
 done
